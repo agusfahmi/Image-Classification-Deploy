6a3e3c581fd56eb31938d3b9cbbd504a05ca9c05
Agus Fahmi <sensai.fahmi1@gmail.com>
2023-12-14 16:02:56 +0700

    change

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def predict_label(img_path):
     model = load_model('model.h5')
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
+    img_array = img_array / 255.0  
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 	
